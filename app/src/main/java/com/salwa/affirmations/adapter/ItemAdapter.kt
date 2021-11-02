4377db360dc522823c3725245bd269a11eb91c7d
salwa abdelkader <abdelkadersalwa21@gmail.com>
2021-11-02 10:50:09 +0000

    commit Final

--- app/src/main/java/com/salwa/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/salwa/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.salwa.affirmations.R
@@ -14,9 +15,10 @@ import com.salwa.affirmations.model.Affirmation
     private val dataset: List<Affirmation>
    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_title)
-    }
+     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+         val textView: TextView = view.findViewById(R.id.item_title)
+         val imageView: ImageView = view.findViewById(R.id.item_image)
+     }
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
          // create a new view
@@ -30,6 +32,7 @@ import com.salwa.affirmations.model.Affirmation
      override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
          val item = dataset[position]
          holder.textView.text = context.resources.getString(item.stringResourceId)
+         holder.imageView.setImageResource(item.imageResourceId)
      }
 
      override fun getItemCount(): Int {
